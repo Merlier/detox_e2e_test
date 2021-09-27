d25bb653160064c03d2f3a69779ffc2690bef820
Nicolas TORRE <n.torre@e2time.com>
2021-09-27 18:14:08 +0200

    Correction tested android version

--- e2e/run_e2e_tests.sh
+++ e2e/run_e2e_tests.sh
@@ -5,8 +5,8 @@ apt update
 npm i -g envinfo detox-cli && envinfo
 npm install
 
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager --channel=0 --verbose "system-images;android-29;default;x86_64"
-echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "Pixel_API_29" --package "system-images;android-29;default;x86_64" --sdcard 200M --device 11
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager --channel=0 --verbose "system-images;android-30;default;x86_64"
+echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "Pixel_API_30" --package "system-images;android-30;default;x86_64" --sdcard 200M --device 11
 
 npx jetify
 npx detox build -c android.emu.release
